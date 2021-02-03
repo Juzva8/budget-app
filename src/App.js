0fda2347283d074e6047cafba185679e3f8e8aca
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-03 13:27:34 -0800

    creating react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Container } from 'semantic-ui-react'
+import './App.css';
+
+function App() {
+  return (
+
+    <Container>
+      <header as='h1'> Budget </header>
+    </Container>
+  );
+}
+
+export default App;
