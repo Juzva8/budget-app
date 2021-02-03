9507fc6ce2fe4703c30ec63e3cfd890a3900ffa7
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-03 14:29:52 -0800

    added some foundation for code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container, Header, Statistic, Segment, Grid, Icon } from 'semantic-ui-react'
+import { Container, Header, Statistic, Segment, Grid, Icon, Form, Button } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
@@ -41,17 +41,64 @@ function App() {
 <Segment color="red"> 
 <Grid columns={3} textAlign="right">
 <Grid.Row>
-<Grid.Column>Something</Grid.Column>
-<Grid.Column>$10.00</Grid.Column>
-<Grid.Column>
+<Grid.Column width={10} textAlign="left" >Something</Grid.Column>
+<Grid.Column width={3} textAlign="right" >$10.00</Grid.Column>
+<Grid.Column width={3}>
   <Icon name="edit" bordered/>
-  <Icon name="trash" />
+  <Icon name="trash" bordered/>
 </Grid.Column>
 </Grid.Row>
-
-  
 </Grid>
 </Segment>
+<Segment color="red"> 
+<Grid columns={3} textAlign="right">
+<Grid.Row>
+<Grid.Column width={10} textAlign="left" >Something</Grid.Column>
+<Grid.Column width={3} textAlign="right" >$10.00</Grid.Column>
+<Grid.Column width={3}>
+  <Icon name="edit" bordered/>
+  <Icon name="trash" bordered/>
+</Grid.Column>
+</Grid.Row>
+</Grid>
+</Segment>
+<Segment color="red"> 
+<Grid columns={3} textAlign="right">
+<Grid.Row>
+<Grid.Column width={10} textAlign="left" >Something</Grid.Column>
+<Grid.Column width={3} textAlign="right" >$10.00</Grid.Column>
+<Grid.Column width={3}>
+  <Icon name="edit" bordered/>
+  <Icon name="trash" bordered/>
+</Grid.Column>
+</Grid.Row>
+</Grid>
+</Segment>
+
+
+<Header as="h3">Add new transaction</Header>
+<Form unselectable>
+  <Form.Group>
+    <Form.Input 
+    icon="tags" 
+    width={12}
+    label='Description'
+    placeholder="New shinny thing"
+    />
+    <Form.Input 
+    icon='dollar'
+    iconPosition='left'
+    width={4}
+    label='Value'
+    placeholder="100.00"
+    />
+  </Form.Group>
+  <Button.Group style={{marginTop: 20}}>
+    <Button>Cancel</Button>
+    <Button.Or/>
+    <Button primary>OK</Button>
+  </Button.Group>
+</Form>
     </Container>
   );
 }
