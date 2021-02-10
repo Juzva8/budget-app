0e8654cd9ee3d07f58c3ce60853a2cd797ad5d26
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-10 12:44:12 -0800

    adding

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Grid, Icon, Segment } from 'semantic-ui-react';
 import {useDispatch} from 'react-redux';
 import { removeEntryRedux } from '../actions/entries.actions';
-
+import { openEditModal } from '../actions/modals.actions'
 
 function EntryLine({
   id, 
@@ -24,7 +24,7 @@ function EntryLine({
         {value}
             </Grid.Column>
         <Grid.Column width={3}>
-          <Icon name="edit" bordered onClick={() => editEntry(id)}/>
+          <Icon name="edit" bordered onClick={() => dispatch(openEditModal(id))}/>
           <Icon name="trash" bordered onClick={() => dispatch(removeEntryRedux(id))} />
         </Grid.Column>
         </Grid.Row>
