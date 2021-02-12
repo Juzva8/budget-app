2a015129da2485b283ccdc63e56946d825035db0
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-11 16:48:31 -0800

    adding

--- src/components/ModalEdit.js
+++ src/components/ModalEdit.js
@@ -1,35 +1,33 @@
 import { useDispatch } from 'react-redux'
 import { Modal, Button } from 'semantic-ui-react'
 import { closeEditModal } from '../actions/modals.actions';
+import useEntryDetails from '../hooks/useEntryDetails';
 import EntryForm from './EntryForm'
 
 function ModalEdit({
     isOpen, 
-    setIsOpen,
     description, 
-    setDescription, 
     value, 
-    setValue,
     isExpense, 
-    setIsExpense 
     }) {
     const dispatch = useDispatch();
+    const entryUpdate = useEntryDetails(description, value, isExpense)
     return (
         <Modal open = {isOpen} >
             <Modal.Header>Edit entry</Modal.Header>
             <Modal.Content>
              <EntryForm 
-                description={description}
-                setDescription={setDescription}
-                value={value}
-                setValue={setValue}
-                isExpense={isExpense}
-                setIsExpense={setIsExpense}
+                description={entryUpdate.description}
+                setDescription={entryUpdate.setDescription}
+                value={entryUpdate.value}
+                setValue={entryUpdate.setValue}
+                isExpense={entryUpdate.isExpense}
+                setIsExpense={entryUpdate.setIsExpense}
              />
                 </Modal.Content>
                 <Modal.Actions> 
                 <Button onClick={() => dispatch(closeEditModal())}>Close</Button>
-                <Button onClick={() => setIsOpen(false)} primary>OK</Button>
+                <Button onClick={() => dispatch(closeEditModal())} primary>OK</Button>
                 </Modal.Actions>
         </Modal>
     )
